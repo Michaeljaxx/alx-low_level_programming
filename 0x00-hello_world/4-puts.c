56487d66160aecd374e9a2a49f967c3f3a75899d
Michaeljaxx <ikejiakumichael99@gmail.com>
2022-09-10 13:57:52 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
 * main - print the string in the put function
 *
-* Descriptin: using the main function
-* this progran print "programming is like building a multilingual puzzle
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
 * retun: 0
 */
 int main(void)
